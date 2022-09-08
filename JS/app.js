1eb1cd419a07aa2c707dd1afb9d4910bba134e46
MdavidL <david.lapeyronnie@lapiscine.pro>
2022-09-08 21:42:05 +0200

    HTML ok

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,7 @@
 const menuBtnProfile = document.querySelector(".profile .profile__btn--menu");
 const crossProfileClosed = document.querySelector(".sidebar__profile--cross");
+const profileToCareer = document.getElementById("profile-to-career");
+const profileToSkills = document.getElementById("profile-to-skills");
 
 menuBtnProfile.addEventListener('click', () => {
     document.querySelector(".sidebar__profile").style.display = "block";
@@ -7,7 +9,7 @@ menuBtnProfile.addEventListener('click', () => {
     document.querySelector(".profile .profile__elements").style.filter = "blur(5px)";
     document.querySelector(".footer").style.filter = "blur(5px)";
     document.querySelector("body").addEventListener('wheel', preventScroll, {passive: false});
-    
+
     function preventScroll(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -29,18 +31,20 @@ crossProfileClosed.addEventListener('click', () => {
     crossProfileClosed.addEventListener('click', location.reload(), false);
 })
 
+profileToCareer.addEventListener('click', () => {
+    document.querySelector(".header h1").style.filter = "none";
+    profileToCareer.addEventListener('click', location.reload(), false);
+})
 
-
-//document.querySelector(".sidebar__link").onclick = function () {
-    //function sidebarItemSelected() {
-        //document.querySelector(".footer").style.filter = "blur(1px)";
-    //}
-    //sidebarItemSelected();
-//};
-
+profileToSkills.addEventListener('click', () => {
+    document.querySelector(".header h1").style.filter = "none";
+    profileToSkills.addEventListener('click', location.reload(), false);
+})
 
 const menuBtnCareer = document.querySelector(".career .career__btn--menu");
 const crossCareerClosed = document.querySelector(".sidebar__career--cross");
+const careerToProfile = document.getElementById('career-to-profile');
+const careerToSkills = document.getElementById('career-to-skills');
 
 menuBtnCareer.addEventListener('click', () => {
     document.querySelector(".sidebar__career").style.display = "block";
@@ -71,9 +75,21 @@ crossCareerClosed.addEventListener('click', () => {
     crossCareerClosed.addEventListener('click', location.reload(), false);
 })
 
+careerToProfile.addEventListener('click', () => {
+    document.querySelector(".header h1").style.filter = "none";
+    careerToProfile.addEventListener('click', location.reload(), false);
+})
+
+careerToSkills.addEventListener('click', () => {
+    document.querySelector(".header h1").style.filter = "none";
+    careerToSkills.addEventListener('click', location.reload(), false);
+})
+
 
 const menuBtnSkills = document.querySelector(".skills .skills__btn--menu");
 const crossSkillsClosed = document.querySelector(".sidebar__skills--cross");
+const skillsToProfile = document.getElementById("skills-to-profile");
+const skillsToCareer = document.getElementById("skills-to-career");
 
 menuBtnSkills.addEventListener('click', () => {
     document.querySelector(".sidebar__skills").style.display = "block";
@@ -103,5 +119,15 @@ menuBtnSkills.addEventListener('click', () => {
 
 crossSkillsClosed.addEventListener('click', () => {
     crossSkillsClosed.addEventListener('click', location.reload(), false);
+})
+
+skillsToProfile.addEventListener('click', () => {
+    document.querySelector(".header h1").style.filter = "none";
+    skillsToProfile.addEventListener('click', location.reload(), false);
+})
+
+skillsToCareer.addEventListener('click', () => {
+    document.querySelector(".header h1").style.filter = "none";
+    skillsToCareer.addEventListener('click', location.reload(), false);
 })
 
